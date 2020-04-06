80dd2b5b1b277818e135689723587a5a98243878
Ish <ish23.siva@gmail.com>
2020-04-06 18:19:11 +0530

    added the constant folding module

---
+++ const_folding.py
@@ -0,0 +1,38 @@
+import re
+
+
+class ConstantFolding:
+	def __init__(self, fname):
+		self.fname = fname
+
+
+	def run_optim(self):
+
+		f = open(self.fname, "r")
+		lines = f.readlines()
+		f.close()
+
+		pat ="([0-9]+[\+\-\*\/][0-9]+([\+\-\*\/][0-9]+)*)"
+		pat = re.compile(pat)
+
+		for j in range(len(lines)):
+
+			if(pat.search(lines[j])):
+				s = lines[j]
+				lst = pat.findall(s)
+				
+
+				for i in lst:
+					olstr = i[0]
+
+					nstr = eval(olstr)
+					
+					s = s.replace(olstr, str(nstr))
+			
+				lines[j] = s
+
+
+		f= open("optim3.py", "w")
+		f.writelines(lines)
+		f.close()
+
--- runner.py
+++ runner.py
@@ -1,9 +1,32 @@
 from constant_propagation import ConstantPropagation as cp
 from strength_red import StrengthReduction as sr
+from const_folding import ConstantFolding as cf
 
 strred = sr("test2.py")
 strred.run_optim()
+print("completed strength reduction")
 
 consprop = cp("optim2.py")
 consprop.run_optim()
+print("completed constant_propagation")
 
+consfold = cf("optimizer.py")
+consfold.run_optim()
+print("completed const_folding")
+
+
+consprop = cp("optim3.py")
+consprop.run_optim()
+
+print("completed second epoch of constant_propagation")
+
+
+consfold = cf("optimizer.py")
+consfold.run_optim()
+print("completed second epoch of const_folding")
+
+
+consprop = cp("optim3.py")
+consprop.run_optim()
+
+print("completed third epoch of constant_propagation")
