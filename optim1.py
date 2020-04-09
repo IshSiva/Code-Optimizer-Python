06f7664f64eb532162c2f0bdae49cf7a64591d74
Ish <ish23.siva@gmail.com>
2020-04-09 22:24:19 +0530

    Final commit

--- optim1.py
+++ optim1.py
@@ -1,29 +1,27 @@
-hello("jkjwe")
 for i in range(10):
     print("a")
 for j in range(10):
     m=m*5
     print("Second loop")
 c=10
-hi
 c=23
 d=242343
 print("hello")
-for c in l:
+for a in range(0,10):
     print("A different one!")
 f=c/3
 d=d+78
 s="eee"
-for c in l:
-    print("One more")
 s1=s
-6=3
-for j in range(len(c)):
-    p+=4
-for c in h:
+h=78
+for a in range(0,5):
     print("Different data structure")
 a=b+c+g
 b=b+c
 d=b+c
 f=a+b-c+d
 g=a+b-c+d
+i=b+h
+j=i+c
+
+
--- optimizer.py
+++ optimizer.py
@@ -1,29 +1,27 @@
-hello("jkjwe")
 for i in range(10):
     print("a")
 for j in range(10):
     m=m+m+m+m+m
     print("Second loop")
 c=10
-hi
 c=23
 d=242343
 print("hello")
-for c in l:
+for a in range(0,10):
     print("A different one!")
 f=7.666666666666667
 d=242421
 s="eee"
-for c in l:
-    print("One more")
 s1="eee"
-6=3
-for j in range(len(c)):
-    p+=4
-for c in h:
+h=78
+for a in range(0,5):
     print("Different data structure")
 a=b+23+g
 b=b+23
 d=b+23
 f=b+23+g+b-23+b+23
 g=b+23+g+b-23+b+23
+i=b+78
+j=b+101
+
+
